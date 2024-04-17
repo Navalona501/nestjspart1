07f843d5d07755dfd60c8e7b14946d252bf40233
Navalona501 <119346839+Navalona501@users.noreply.github.com>
2024-04-17 20:56:35 +0300

    initial commit

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,33 @@
+import { UseInterceptors
+    , NestInterceptor
+    , ExecutionContext
+    , CallHandler
+} from "@nestjs/common";
+
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { plainToClass, plainToInstance } from "class-transformer";
+import { UserDto } from "src/users/dtos/user.dto";
+
+export function Serialize(dto: any){
+    return UseInterceptors(new SerializeInterceptor(dto))
+}
+
+export class SerializeInterceptor implements NestInterceptor{
+    constructor(private dto: any) {}
+
+    intercept(context: ExecutionContext, handler: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+        
+
+        return handler.handle().pipe(
+            map((data: any) => {
+                return plainToInstance(this.dto, data, {
+                    excludeExtraneousValues: true
+                })
+
+            })
+
+        )
+    }
+
+}
